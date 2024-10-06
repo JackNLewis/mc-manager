28e29e08b66665948e71ad7069f5cfb10c919e88
jack lewis <jacklewis@jacks-Laptop.local>
2024-10-06 18:10:26 +0100

    updated log container to scroll to bottom

--- src/components/logcontainer.tsx
+++ src/components/logcontainer.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import useWebSocket from "react-use-websocket";
-import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface InfoSectionProps {
 	ipAddress: string;
@@ -9,24 +8,39 @@ interface InfoSectionProps {
 
 export function LogContainer({ ipAddress }: InfoSectionProps) {
 	const url = `ws://${ipAddress}:8080/ws`
-	const { lastMessage } =  useWebSocket(url)
+	const { lastMessage } = useWebSocket(url);
 	const [messageHistory, setMessageHistory] = useState("");
+	const scrollRef = useRef<HTMLInputElement>(null);
 
+
+	// Update message History from websocket
 	useEffect(() => {
 		if (lastMessage !== null) {
-			setMessageHistory( (message) =>  message + lastMessage.data);
+			setMessageHistory((message) => message + lastMessage.data);
 		}
+
 	}, [lastMessage]);
-	
+
+	// Keep message log scrolled at bottom after every re-render
+	useEffect(() => {
+		if (scrollRef.current){
+			scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+
+		}
+	}, [messageHistory]);
+
 	return (
 		<div className="w-11/12 h-full mx-auto py-10">
-			<ScrollArea className="flex bg-card w-full h-full rounded-lg rounded-lg border text-card-foreground shadow-sm whitespace-pre p-4">
-				{messageHistory}
-			</ScrollArea>
+			<div
+				ref={scrollRef}
+				className="relative flex bg-card w-full h-full rounded-lg rounded-lg border text-card-foreground shadow-sm whitespace-pre p-4 overflow-y-scroll text-wrap"
+			>
+				{messageHistory}	
+			</div>
+
 		</div>
 	);
 }
-
 
 export function LogContainerEmpty() {
 	return (
